6fea07b6a30e09508e21ed326afc79bc780e13d5
melvinkukwansafari <mevin.kukwan@student.moringaschool.com>
2023-02-17 12:20:24 +0300

    complete assignment

--- Gemfile
+++ Gemfile
@@ -2,10 +2,12 @@
 source "https://rubygems.org"
 
 # Database
-gem 'sqlite3', '~>1.4'
+gem 'sqlite3', '~>1.4.2'
+
 
 # Testing
 gem 'rspec'
 
 # Debugging
 gem 'pry'
+
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 12, 'M', 'brown', 'grumpy', true),
+  ('Rowdy', 6, 'M', 'black', 'playful', true),
+  ('Tabitha', 3, 'F', 'white', 'shy', true),
+  ('Sergeant Brown', 7, 'M', 'brown', 'brave', true),
+  ('Melissa', 4, 'F', 'gray', 'calm', true),
+  ('Grinch', 1, 'M', 'green', 'mischievous', true),
+  ('Wendy', 2, 'F', 'brown', 'curious', true),
+  (NULL, NULL, 'F', NULL, NULL, true);
